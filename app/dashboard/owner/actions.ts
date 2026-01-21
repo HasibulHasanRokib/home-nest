72fce7a3277b8ffef48e6b45122ac39dd847b8e9
Hasibul Hasan Rokib <rokib4000@gmail.com>
2026-01-22 00:55:20 +0600

    Add booking status email template and integrate email notifications for booking approval and rejection

--- app/dashboard/owner/actions.ts
+++ app/dashboard/owner/actions.ts
@@ -6,6 +6,10 @@ import { Role } from "@/lib/generated/prisma/enums";
 import { prisma } from "@/lib/prisma";
 import { propertySchema } from "@/lib/zod-schema/property-schema";
 import { revalidatePath } from "next/cache";
+import { Resend } from "resend";
+import BookingStatusEmailTemplate from "./rent-requests/booking-status-email-templete";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function approveBooking(id: string) {
   const user = await getCurrentUser();
@@ -14,6 +18,11 @@ export async function approveBooking(id: string) {
 
   const booking = await prisma.bookingRequest.findUnique({
     where: { id },
+    include: {
+      owner: true,
+      property: true,
+      tenant: true,
+    },
   });
 
   if (!booking) return { error: "Booking not found" };
@@ -28,6 +37,20 @@ export async function approveBooking(id: string) {
     data: { status: "APPROVED" },
   });
 
+  const { data, error } = await resend.emails.send({
+    from: "HomeNest <onboarding@resend.dev>",
+    to: ["delivered@resend.dev"],
+    subject: "Approved Booking request",
+    react: BookingStatusEmailTemplate({
+      propertyName: booking.property.title,
+      tenantName: booking.tenant.name,
+      ownerName: booking.owner.name,
+      status: "APPROVED",
+    }),
+  });
+
+  console.log({ data, error });
+
   revalidatePath("/dashboard/owner/rent-requests");
   return { success: "Booking approved successfully" };
 }
@@ -39,6 +62,11 @@ export async function rejectBooking(id: string) {
 
   const booking = await prisma.bookingRequest.findUnique({
     where: { id },
+    include: {
+      owner: true,
+      property: true,
+      tenant: true,
+    },
   });
 
   if (!booking) return { error: "Booking not found" };
@@ -52,6 +80,19 @@ export async function rejectBooking(id: string) {
     where: { id },
     data: { status: "REJECTED" },
   });
+
+  const { data, error } = await resend.emails.send({
+    from: "HomeNest <onboarding@resend.dev>",
+    to: ["delivered@resend.dev"],
+    subject: "Rejected Booking request",
+    react: BookingStatusEmailTemplate({
+      propertyName: booking.property.title,
+      tenantName: booking.tenant.name,
+      ownerName: booking.owner.name,
+      status: "REJECTED",
+    }),
+  });
+  console.log({ data, error });
   revalidatePath("/dashboard/owner/rent-requests");
   return { success: "Booking rejected" };
 }
@@ -74,7 +115,7 @@ export async function deletePropertyAction(propertyId: string) {
 
   if (!propertyExists) {
     throw new Error(
-      "Property not found or you do not have permission to delete it"
+      "Property not found or you do not have permission to delete it",
     );
   }
 
